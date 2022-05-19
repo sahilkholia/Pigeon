5c406a9aea018d21e26fa179fa4472c1a0ec127d
sahil <sahilkholia9@gmail.com>
2022-05-19 10:09:15 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserCategoryComponent } from './user-category/user-category.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login', component:UserCategoryComponent},
+  { path: '', redirectTo: '/login', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/user-category/user-category.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-category',
+  templateUrl: './user-category.component.html',
+  styleUrls: ['./user-category.component.css']
+})
+export class UserCategoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  sellerLogin(){
+    alert("Seller Login Clicked!!!!");
+  }
+
+}
